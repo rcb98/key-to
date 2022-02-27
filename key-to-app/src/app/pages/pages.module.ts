51e9694980923c4f86d73ce37ef376008ebd592b
Raquel Copete Ballester <raquelcb98@gmail.com>
2022-02-27 17:37:34 +0100

    Menu done

--- key-to-app/src/app/pages/pages.module.ts
+++ key-to-app/src/app/pages/pages.module.ts
@@ -5,16 +5,16 @@ import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserDetailPageComponent } from './user-detail-page/user-detail-page.component';
 import { MenuComponent } from '../components/menu/menu.component';
-import { SubMenuComponent } from '../components/sub-menu/sub-menu.component';
 import { LateralMenuComponent } from '../components/lateral-menu/lateral-menu.component';
 import { UserDetailComponent } from '../components/user-detail/user-detail.component';
 import { BottomActionsComponent } from '../components/bottom-actions/bottom-actions.component';
+import { HeaderComponent } from '../components/header/header.component';
 
 @NgModule({
   declarations: [
     UserDetailPageComponent,
+    HeaderComponent,
     MenuComponent,
-    SubMenuComponent,
     LateralMenuComponent,
     UserDetailComponent,
     BottomActionsComponent
